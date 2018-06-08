8579d162ca192448f9bcecc5783ab5f924f7f2a3
xuqiang <publicqiang@163.com>
2018-06-08 13:09:09 +0800

    ssm整合

---
+++ src/main/java/com/mySSM/user/bean/User.java
@@ -0,0 +1,43 @@
+package com.mySSM.user.bean;
+
+public class User {
+    private String id;
+
+    private String userName;
+
+    private String password;
+
+    private Short state;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Short getState() {
+        return state;
+    }
+
+    public void setState(Short state) {
+        this.state = state;
+    }
+}
---
+++ src/main/java/com/mySSM/user/dao/UserMapper.java
@@ -0,0 +1,19 @@
+package com.mySSM.user.dao;
+
+import com.mySSM.user.bean.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
